9ae25fc7cb0dd0e475d2609f1e4a28b73692c7f7
xusanyusupov <yusupov.xusan.org@gmail.com>
2025-08-26 16:26:02 +0500

    add Mobile App Alert

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,14 +1,18 @@
 import TopBar from "@/components/Header/components/TopBar"
 import SearchBar from "@/components/Header/components/SearchBar"
+import MobileBanner from "../MobileAppPromotion/MobileBanner"
 
-const Index = () => {
+const Index = () => {  
+  
+     
   return (
     <>
     <header>
       <div className="relative">
         <TopBar/>
         <SearchBar/>
-     </div>
+        <MobileBanner/> 
+      </div>
     </header>
     </>
   )
---
+++ src/components/MobileAppPromotion/MobileBanner.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import { FaStar, FaXmark } from "react-icons/fa6"
+import logoMobile from '@/assets/logo_modile_app.svg'
+
+const MobileBanner = () => {
+    const [showMobileApp,setShowMobileApp] = useState(false)
+    useEffect(() => {
+        setTimeout(() => {
+            setShowMobileApp(true)
+        }, 4000);
+    },[])
+  return (
+    <>
+        {
+            showMobileApp && (
+                <div className="w-full h-32 fixed bottom-0 left-0 z-10 bg-white flex items-center justify-between max-sm:h-24">
+                    <div className="flex flex-col items-start gap-2 px-5 max-sm:px-2">
+                        <div className="flex items-center gap-5 max-sm:gap-2">
+                            <div className="w-10 h-10 flex items-center justify-center border-2 p-2 rounded-md max-sm:w-9 max-sm:h-9">
+                                <img src={logoMobile} alt="Mobile_app_logo" />
+                            </div>
+                            <div className="flex flex-col ">
+                                <p className="font-bold flex items-center gap-3 max-sm:text-sm">ASOS <FaStar className="text-yellow-500"/><FaStar className="text-yellow-500"/><FaStar className="text-yellow-500"/><FaStar className="text-yellow-500"/><FaStar className="text-yellow-500"/></p>
+                                <span>Avilable in the play store</span>
+                            </div>
+                        </div>
+                       <div className="flex items-center gap-5 max-sm:gap-2">
+                        <button className="bg-slate-100 inter px-8 py-2 rounded-sm max-sm:px-4 whitespace-nowrap max-sm:py-1 max-sm:text-sm">VIEW IN APP</button>
+                        <button className="bg-green-700 inter px-8 py-2 rounded-sm tex-white max-sm:px-4 whitespace-nowrap max-sm:py-1 max-sm:text-sm">GET APP</button>
+                       </div>
+                    </div>
+                    <button className="px-5" onClick={() => setShowMobileApp(false)}><FaXmark className="text-3xl max-sm:text-xl"/></button>
+                </div>
+            )
+        }
+    </>
+  )
+}
+
+export default MobileBanner
